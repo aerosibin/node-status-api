4e00a1fb2163ec5049051bfb4ab1ebcfa5a8ad0b
aerosibin <aerosibin777@gmail.com>
2026-09-21 17:58:49 +0530

    web hook test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/status', (req, res) => {
     res.json({
         status: "success",
-        message: "The Node.js API is running smoothly.",
+        message: "The Node.js API is running smoothly. Testing webhook",
         timestamp: new Date().toISOString()
     });
 });
